5918472afb03a24abe50705d2096b8d443af738b
mirza-1 <mirzasyarif1@gmail.com>
2021-11-05 14:41:50 +0700

    ngubah family status page

--- resources/views/fs.blade.php
+++ resources/views/fs.blade.php
@@ -11,29 +11,112 @@
         </div>
         <div class="card-body">
           <div class="form-group">
-            <button type="submit" class="btn btn-primary btn-lg mb-3" tabindex="4">
-              Add Family Status
-            </button>
+
           <div class="table-responsive">
             <table class="table table-striped" id="table-1">
               <thead>                                 
                 <tr>
                   <th class="text-center">
-                    ID
+                    Family Status (PTKP)
                   </th>
-                  <th>Name</th>
+                  <th>Type</th>
                   <th>Value</th>
-                  <th>Members</th>
+                  <th>Employee</th>
+                  <th>Employee ID</th>
                 </tr>
               </thead>
               <tbody>                                 
                 <tr>
                   <td class="text-center">
-                    1
+                    TK0
                   </td>
-                  <td>Create a mobile app</td>
+                  <td>Not Married, No Responsibilities</td>
                   <td>
-                    Rp. 500.000
+                    Rp. 54.000.000
+                  </td>
+                  <td>
+                    Mamang Kesbor, KimKim, Putra
+                  </td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    TK1
+                  </td>
+                  <td>Not Married, 1 Responsibility</td>
+                  <td>
+                    Rp. 58.500.000
+                  </td>
+                  <td>
+                    Mamang Kesbor, KimKim, Putra
+                  </td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    TK2
+                  </td>
+                  <td>Not Married, 2 Responsibilities</td>
+                  <td>
+                    Rp. 63.000.000
+                  </td>
+                  <td>
+                    Mamang Kesbor, KimKim, Putra
+                  </td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    TK3
+                  </td>
+                  <td>Not Married, 3 Responsibilities</td>
+                  <td>
+                    Rp. 67.500.000
+                  </td>
+                  <td>
+                    Mamang Kesbor, KimKim, Putra
+                  </td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    K0
+                  </td>
+                  <td>Married, No Responsibilities</td>
+                  <td>
+                    Rp. 58.500.000
+                  </td>
+                  <td>
+                    Mamang Kesbor, KimKim, Putra
+                  </td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    K1
+                  </td>
+                  <td>Married, 1 Responsibility</td>
+                  <td>
+                    Rp. 63.000.000
+                  </td>
+                  <td>
+                    Mamang Kesbor, KimKim, Putra
+                  </td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    K2
+                  </td>
+                  <td>Married, 2 Responsibilities</td>
+                  <td>
+                    Rp. 67.500.000
+                  </td>
+                  <td>
+                    Mamang Kesbor, KimKim, Putra
+                  </td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    K3
+                  </td>
+                  <td>Married, 3 Responsibilities</td>
+                  <td>
+                    Rp. 72.000.000
                   </td>
                   <td>
                     Mamang Kesbor, KimKim, Putra
